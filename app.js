c08f2aaaceabb35ba52970a21be96e91a9425beb
luizrobertoao <luizrobertoao29@gmail.com>
2020-02-21 15:52:16 -0300

    Subindo um servidor com framework Express

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 let app = express();
+
+// Subindo um servidor com Express
+
+app.get('/', (req, res) => res.send('Olá Mundo!'));
+
+app.listen(3000, () => console.log('Servidor rodando na porta 3000!'))
