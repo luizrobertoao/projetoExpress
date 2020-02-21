21401eccc12ee0e1e88902c3cab9704783305a4c
luizrobertoao <luizrobertoao29@gmail.com>
2020-02-21 17:48:06 -0300

    Adicionando rotas parametrizadas.

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ let app = express();
 app.get('/', (req, res) => res.send('Olá Mundo!'));
 app.get('/contatos', (req, res) => res.send({nome: 'Luiz', idade: 22}));
 
+// Rotas parametrizadas
+
+app.get('/produtos/:id', (req, res) => {
+    let id = req.params.id;
+    // usando desestruturação let {id} = req.params;
+    console.log('Eu tenho um produto com o id: ' + id);
+    res.send('Eu tenho um produto com o id: ' + id);
+});
+
+// ***SERVIDOR***
 app.listen(3000, () => console.log('Servidor rodando na porta 3000!'))
+
+
+
