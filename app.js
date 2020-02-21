ca91eb695909964f524774a340c68563363b56f2
luizrobertoao <luizrobertoao29@gmail.com>
2020-02-21 16:31:49 -0300

    Criando rotas no framework Express.

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ let app = express();
 // Subindo um servidor com Express
 
 app.get('/', (req, res) => res.send('Olá Mundo!'));
+app.get('/contatos', (req, res) => res.send({nome: 'Luiz', idade: 22}));
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000!'))
